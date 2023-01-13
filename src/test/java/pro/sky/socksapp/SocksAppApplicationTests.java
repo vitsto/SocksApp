e415841854d225ea8bc107168677e9cb470508ef
vitsto <stolyarov.vitalij@list.ru>
2023-01-13 03:12:30 +0300

    initial commit

---
+++ src/test/java/pro/sky/socksapp/SocksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.socksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
